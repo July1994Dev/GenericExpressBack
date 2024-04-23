32f33c2dbff386978c04469b6c747f1745df3d4d
XID-JulyDev <juliotoluca.1994@gmail.com>
2024-04-23 00:20:57 -0600

    cambios

--- src/utils/nodemailerEmailSender.ts
+++ src/utils/nodemailerEmailSender.ts
@@ -6,15 +6,12 @@ export class NodemailerEmailSender implements IEmailSender {
     private transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: Config.get('GMAIL_USER'),
-            pass: Config.get('GMAIL_PASS')
+            user: 'juliotoluca.1994@gmail.com',
+            pass: 'xqnr eqrf bcaq dijm'
         }
     });
 
     async sendMail(to: string | string[], subject: string, body: string): Promise<void> {
-        console.log(Config.get('GMAIL_USER'));
-        console.log(Config.get('GMAIL_PASS'));
-
         const mailOptions = {
             from: Config.get('GMAIL_PASS'),
             to: to,
